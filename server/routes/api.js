d8211536b7d79f5319194f3d9fe27345f8843210
mariajcb <bogomazm@gmail.com>
2016-11-06 15:05:24 -0700

    full CRUD

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,21 +5,48 @@ var router = express.Router();
 var knex = require(`../db/knex`);
 
 router.get('/', (req, res, next) => {
-  console.log("GETTING ALL===========");
   knex('posts')
   .then(posts => {
     res.json(posts)
   })
 });
 
+router.get(`/:id`, function(req, res, next) {
+    knex(`posts`)
+        .where(`id`, req.params.id)
+        .first()
+        .then(function(post) {
+            res.json(post)
+        })
+})
+
 router.post(`/`, (req, res, next) => {
-    console.log(`POSTING===============`);
     knex(`posts`)
         .insert(req.body)
         .returning(`*`)
         .then(function(newPost) {
-            console.log(newPost);
             res.json(newPost)
+        })
+})
+
+router.put(`/:id`, function(req, res, next) {
+  console.log(`EDITING=======`);
+    knex(`posts`)
+        .where(`id`, req.params.id)
+        .first()
+        .update(req.body)
+        .then(function(post) {
+            res.json('post updated')
+        })
+})
+
+router.delete(`/:id`, function(req, res, next) {
+    knex(`posts`)
+        .where(`id`, req.params.id)
+        .first()
+        .del()
+        .then(function() {
+            res.json('post deleted')
         })
 })
 
