05d718dd845e8af84272bd594eae48d07a5f99ca
mariajcb <bogomazm@gmail.com>
2016-11-09 11:51:48 -0700

    add and delete comments

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,27 +3,26 @@
 var express = require('express');
 var router = express.Router();
 var knex = require(`../db/knex`);
-var bcrypt = require('bcrypt')
 
 router.get('/', (req, res, next) => {
-  knex('posts')
-  .then(posts => {
-    knex('comments')
-    .then(comments => {
-      posts.forEach(post => {
-        post.comments = []
-        comments.forEach(comment => {
-          post.id == comment.posts_id ? post.comments.push(comment) : null;
+    console.log(`GET ROUTE IS FIRING`);
+    knex('posts')
+        .then(posts => {
+            knex('comments')
+                .then(comments => {
+                    posts.forEach(post => {
+                        post.comments = []
+                        comments.forEach(comment => {
+                            post.id == comment.posts_id ? post.comments.push(comment) : null;
+                        })
+                    })
+                    res.json(posts)
+                })
         })
-      })
-      console.log('posts with comments', posts);
-      res.json(posts)
-    })
-  })
 });
 
 router.get(`/:id`, function(req, res, next) {
-  console.log(`WENT TO THE POSTS ID ROUTE`);
+    console.log(`WENT TO THE POSTS ID ROUTE`);
     knex(`posts`)
         .where(`id`, req.params.id)
         .first()
@@ -41,6 +40,15 @@ router.post(`/`, (req, res, next) => {
         })
 })
 
+router.post(`/addcomment`, (req, res, next) => {
+    knex(`comments`)
+        .insert(req.body)
+        .returning(`*`)
+        .then(function(newComment) {
+            res.json(newComment)
+        })
+})
+
 router.put(`/:id`, function(req, res, next) {
   console.log(`EDITING=======`);
     knex(`posts`)
@@ -52,8 +60,20 @@ router.put(`/:id`, function(req, res, next) {
         })
 })
 
+
 router.delete(`/:id`, function(req, res, next) {
     knex(`posts`)
+        .where(`id`, req.params.id)
+        .first()
+        .del()
+        .then(function() {
+            res.json('post deleted')
+        })
+})
+
+router.delete(`/deletecomment/:id`, function(req, res, next) {
+    console.log(`DELETE COMMENT ROUTE IS FIRING, commentID:`, req.params.id);
+    knex(`comments`)
         .where(`id`, req.params.id)
         .first()
         .del()
