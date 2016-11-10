1ebfb4187b0a7f5c150a177f5ef5007b1fe3541d
mariajcb <bogomazm@gmail.com>
2016-11-10 10:19:43 -0700

    fifth commit

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-require(`dotenv`).load()
-
 var express = require(`express`);
 var path = require(`path`);
 var cookieParser = require(`cookie-parser`);
--- server/db/knex.js
+++ server/db/knex.js
@@ -1,3 +1,5 @@
+require(`dotenv`).load()
+
 var environment = process.env.NODE_ENV || `development`
 var config = require(`../knexfile.js`)[environment]
 var knex = require(`knex`)(config)
